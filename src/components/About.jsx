74d8c880081d8a07c53a180c45799b7783d8b23b
Wasif-Web-Dev <itswasif.dev@gmail.com>
2024-11-01 23:24:39 +0500

    sette

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-  return (
-    <div>About</div>
-  )
+    return <div className="text-white text-4xl mx-auto">About</div>;
 }
 
-export default About
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Contact() {
-    return <div></div>;
+    return <div className="text-white text-4xl mx-auto">Contact</div>;
 }
 
 export default Contact;
