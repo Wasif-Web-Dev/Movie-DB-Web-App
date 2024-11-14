8637aabc8fa01093ee738e30e6b84917d0fb4aa9
Wasif-Web-Dev <itswasif.dev@gmail.com>
2024-11-14 22:48:23 +0500

    search-fix

--- src/partials/Topnav.jsx
+++ src/partials/Topnav.jsx
@@ -35,12 +35,12 @@ function topnav({toggleSidebar}) {
     }, [query, searches]);
 
     return (
-        <div className=" flex items-center justify-start px-[20%] gap-5 h-[10vh] relative ">
+        <div className=" flex items-center justify-start px-[20%] gap-5 h-[10vh] max-mobileXL:h-[6vh] relative ">
             <i
-                 onClick={toggleSidebar}
+                onClick={toggleSidebar}
                 class="ri-align-justify z-[46] text-3xl hidden max-md:block text-white fixed top-0 left-5 max-[425px]:left-2  max-[425px]:text-2xl  max-[375px]:left-0"
             ></i>
-            <i className="nav-search_animate text-3xl text-zinc-300 ri-search-line"></i>
+            <i className="nav-search_animate text-3xl max-mobileXL:text-2xl text-zinc-300 ri-search-line"></i>
             <input
                 onChange={(e) => setquery(e.target.value)}
                 value={query}
@@ -49,17 +49,20 @@ function topnav({toggleSidebar}) {
                 placeholder="Search..."
             />
             {query.length > 0 && (
-                <i onClick={() => setquery("")} className="cursor-pointer text-3xl text-zinc-300 ri-close-line"></i>
+                <i
+                    onClick={() => setquery("")}
+                    className="cursor-pointer text-3xl max-mobileXL:text-2xl text-zinc-300 ri-close-line"
+                ></i>
             )}
 
-            <div className="rounded-lg max-h-[50vh] overflow-auto bg-zinc-200 w-[34vw]  absolute top-[90%] max-:w-[40vw]">
+            <div className="rounded-lg max-h-[50vh] overflow-auto bg-zinc-200 w-[34vw] z-[47]  absolute top-[90%] max-tabletM:w-[50vw] max-mobileL:w-[65vw]">
                 {searches.map((e, i) => (
                     <Link
                         to={`/${e.media_type}/details/${e.id}`}
-                        className="my-5 hover:text-black hover:font-semibold bg-zinc-200 text-zinc-600 flex justify-start items-center w-[100%] h-[15vh] p-6 border-b-2 border-zinc-100"
+                        className="my-5 max-sm:h-[10vh] max-sm:my-2 hover:text-black hover:font-semibold bg-zinc-200 text-zinc-600 flex justify-start items-center w-[100%] h-[15vh] p-6 max-sm:p-2 border-b-2 border-zinc-100"
                     >
                         <img
-                            className="mr-10 w-[10vh] h-[10vh] object-cover rounded"
+                            className="mr-10 w-[10vh] h-[10vh] object-cover rounded "
                             src={
                                 e.backdrop_path || e.profile_path
                                     ? `https://image.tmdb.org/t/p/original/${e.backdrop_path || e.profile_path}`
@@ -67,7 +70,7 @@ function topnav({toggleSidebar}) {
                             }
                             alt=""
                         />
-                        <span className="text-md text-start">
+                        <span className="text-md text-start max-sm:text-sm">
                             {e.name || e.title || e.original_names || e.original_title}
                         </span>
                     </Link>
